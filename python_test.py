1c890264ca708aa695a88ec7e314d6cf2ed9273a
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2023-10-10 22:32:40 +0200

    First commit in Arch

--- python_test.py
+++ python_test.py
@@ -23,43 +23,66 @@ import re
 # 	 (958,'611','86','08:16:00','2023-07-01 08:07:00','436',36,13.903000831604004,NULL,'2023-07-01',NULL,NULL,'2023-07-01 08:40:05.827817+02','2023-07-01 08:07:12+02',205,-540,true,'2',NULL,false,false),
 # 	 (958,'611','86','08:17:00','2023-07-01 08:08:12','255',37,14.194000244140625,NULL,'2023-07-01',0,0,'2023-07-01 08:40:05.827817+02','2023-07-01 08:08:23+02',205,-528,true,'2',NULL,false,false);"""
 
-tables = {}
+small = True
 
-with open("rpt_stop_details_202308080948.sql", 'r') as file:
-  print('File caricato')
-  cmds = file.read().split(';')
-  print(f'File letto, sono {len(cmds)} comandi')
+def fileToDict (filename) -> dict:
 
-  for cmd_idx, cmd in enumerate(cmds):
-    if not cmd_idx % 1000: print(f'\rSto parsando la riga {cmd_idx}/{len(cmds)}', end='')
-    if not cmd: continue
-    cmd = cmd.strip()
-    if not cmd or len(cmd) < 20: continue
-    cmd = cmd[19:]
-    table, data = cmd.split(' ', 1)
+  with open(filename, 'r') as file:
+    
+    ltables = {}
+    print(f'File {filename} caricato', end='')
+    cmds = file.read().split(';')
+    print(f'\rFile {filename} letto, sono {len(cmds)} comandi')
 
-    if not table in tables:
-      tables[table] = {
-        'records': [],
-        'legend': None
-      }
+    if small: cmds = cmds[:1000]
 
-    rows = re.findall(r'\((.*?)\)', data)
-    legend = rows[0].split(',')
-    tables[table]['legend'] = legend
-    for row in rows[1:]:
-      record = {}
-      for idx, el in enumerate(row.split(',')):
-        if el == 'NULL': el = None
-        elif el == 'false': el = False
-        elif el == 'true': el = True
-        else: el = eval(el)
-        record[legend[idx]] = el
-      tables[table]['records'].append(record)
+    for cmd_idx, cmd in enumerate(cmds):
+      now_perc = int((cmd_idx + 1) / len(cmds) * 100)
+      prev_perc = int(cmd_idx / len(cmds) * 100)
+      if now_perc != prev_perc: print(f'\rSto parsando la riga {cmd_idx + 1}/{len(cmds)} ({now_perc}%)', end='')
+      if not cmd: continue
+      cmd = cmd.strip()
+      if not cmd or len(cmd) < 20: continue
+      cmd = cmd[19:]
+      table, data = cmd.split(' ', 1)
 
-print()
-media_delay = sum([rec['delay'] for rec in tables['rpt_stop_details']['records'] if rec['delay']]) / len(tables['rpt_stop_details']['records'])
-print(table, media_delay)
+      if not table in ltables:
+        ltables[table] = {
+          'records': [],
+          'legend': None
+        }
+
+      rows = re.findall(r'\((.*?)\)', data)
+      legend = rows[0].split(',')
+      ltables[table]['legend'] = legend
+      for row in rows[1:]:
+        record = {}
+        for idx, el in enumerate(row.split(',')):
+          if el == 'NULL': el = None
+          elif el == 'false': el = False
+          elif el == 'true': el = True
+          else: el = eval(el)
+          record[legend[idx]] = el
+        ltables[table]['records'].append(record)
+  print()
+  return ltables
+
+rpt_stop_details = fileToDict("rpt_stop_details_202308080948.sql")
+prev_stop_details = fileToDict("prev_stop_details_202308080953.sql")
+rpt_trips = fileToDict("rpt_trips_202308080953.sql")
+
+all_data = { **rpt_stop_details, **prev_stop_details, **rpt_trips }
+
+stops = {}
+routes = {}
+for rec in all_data['prev_stop_details']['records']:
+  stops[rec['stop_id']] = rec['stop_name']
+  routes[rec['route_id']] = rec['route_name']
+
+for t,v in all_data.items():
+  # media_delay = sum([rec['delay'] for rec in v['records'] if rec['delay']]) / len(v['records'])
+  # print(t, media_delay)
+  print(t, len(v['records']))
 
 
 
