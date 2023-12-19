f6ad869d95bbeca7abb398f6bc62812d7e119988
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2023-12-19 23:08:15 +0100

    Implementata scrittura su influxdb

--- fillInfluxDB.py
+++ fillInfluxDB.py
@@ -12,11 +12,13 @@ with open('records', 'rb') as file:
 pickle_time_end = time.perf_counter()
 print(f"The loading time for {len(records)} records in python is: {timedelta(seconds = (pickle_time_end - pickle_time_start))}")
 
-records = [rec for rec in records if rec['field'] == 'delay']# rec['delay'] is not None]
 
-
+host = 'http://localhost:8086'
 org = 'vncnz'
 bucket = 'bigdata_project'
+pwd = 'bigdata_project'
+erase_all = True
+skip_write = True
 
 # client = InfluxDBClient(
 #   url="https://europe-west1-1.gcp.cloud2.influxdata.com",
@@ -24,29 +26,47 @@ bucket = 'bigdata_project'
 # )
 client = InfluxDBClient(
     url="http://localhost:8086",
-    token='DA FARE'
+    token='w1vABiIPJ28ixch-pz-DyDDjTlnOpLsxSY8yrUT5dvMi9Xn_wsnDUAU-E4oyTVFhfVHGtqskQRAUm_6LHbZQYA=='
 )
+
+if erase_all:
+    print('Erasing all data in bucket...', end='')
+    start = "1970-01-01T00:00:00Z"
+    stop = "2021-01-01T00:00:00Z"
+    delete_api = client.delete_api()
+    delete_api.delete(start, stop, None, bucket=bucket, org=org) # '_measurement="delay"'
+    print('\rErased all data in bucket!      ')
 
 write_api = client.write_api()
 query_api = client.query_api()
 
-path_linux = '/home/vncnz/TODO'
-path_windows = 'TODO'
+# path_linux = '/home/vncnz/TODO'
+# path_windows = 'TODO'
 
-records = records[:10]
+# records = [rec for rec in records if rec['field'] == 'delay']# rec['delay'] is not None]
+# records = records[:1000]
+
+def recordToGenericPoint (record):
+    return Point("StopCalls") \
+        .tag("day_of_service", record['day_of_service']) \
+        .tag("route_id", record['route_id']) \
+        .tag("trip_id", record['trip_id']) \
+        .tag("stop_id", record['stop_id']) \
+        .tag("block_id", record['block_id']) \
+        .time(record['datetime'], WritePrecision.S)
+
+write_time_start = time.perf_counter()
 
 i = 0
 for record in records:
-    point = Point("StopCalls") \
-            .tag("day_of_service", record['day_of_service']) \
-            .tag("route_id", record['route_id']) \
-            .tag("trip_id", record['trip_id']) \
-            .tag("stop_id", record['stop_id']) \
-            ...
-            .field("delay", record['delay']) \
-            .time(record['...datetime'].isoformat(), WritePrecision.S)
-
-    write_api.write(bucket, org, point)
+    point = recordToGenericPoint(record).field(record['field'], record['value']) # .field("delay", record['delay'])
+    if not skip_write: write_api.write(bucket, org, point)
+    # print(point)
     i += 1
     progressBar(i, len(records), 40)
 print('')
+
+write_time_end = time.perf_counter()
+diff = write_time_end - write_time_start
+# print(f"The written time for {len(records)} records in influxdb is: {timedelta(seconds = diff)}")
+print(f"The written time for {len(records)} records in influxdb is: {timedelta(seconds = diff)} ({(len(records) / diff):.2f} records per second)")
